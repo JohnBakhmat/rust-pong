a8586dda47d2876dd3a1cd1bd83024b953d01c3e
JohnBakhmat <johnbakhmat@gmail.com>
2023-05-18 12:30:36 +0300

    Implemented paddle movement and ball bounce.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-use bevy::prelude::*;
+use bevy::{prelude::*, utils::petgraph::Direction};
 
 fn main() {
 
@@ -10,6 +10,8 @@ fn main() {
         .add_system(print_ball_info)
         .add_system(ball_movement)
         .add_system(ball_limits)
+        .add_system(paddle_movement)
+        .add_system(hit_ball)
         .run();
 }
 
@@ -19,7 +21,7 @@ fn hello_world(){
 }
 
 
-fn spawn_ball(mut commands: Commands){
+fn spawn_ball(commands:&mut Commands){
     commands.spawn((Ball{
         position: Vector2{
             x:0.0,
@@ -41,9 +43,77 @@ fn spawn_ball(mut commands: Commands){
                   );
 }
 
+
+fn spawn_players(commands:&mut Commands){
+    commands.spawn((
+            SpriteBundle{
+                sprite: Sprite{
+                    color: Color::rgb(1.,1.,1.),
+                    custom_size: Some(Vec2::new(20.,120.)),
+                    ..default()
+                },
+                transform: Transform::from_xyz(-500.0, 0.0, 0.0),
+                ..default()   
+            },
+            Paddle{
+                speed:10.0,
+                position:Vector2{x:-500.0,y:0.0},
+                is_player:true
+            }
+            ));
+    commands.spawn((
+            SpriteBundle{
+                sprite: Sprite{
+                    color: Color::rgb(1.,1.,1.),
+                    custom_size: Some(Vec2::new(20.,120.)),
+                    ..default()
+                },
+                transform: Transform::from_xyz(500.0, 0.0, 0.0),
+                ..default()   
+            },
+            Paddle{
+
+                speed:10.0,
+                position:Vector2{x:500.0,y:0.0},
+                is_player:false
+            }
+            ));
+}
+
+fn paddle_movement(keyboard_input:Res<Input<KeyCode>>, mut query: Query<(&mut Paddle, &mut Transform)>){
+
+   for (mut paddle,mut transform) in query.iter_mut(){
+        if paddle.is_player {
+            if keyboard_input.pressed(KeyCode::W) {
+                paddle.move_paddle(Directions::Up);
+            }
+            if keyboard_input.pressed(KeyCode::S) {
+                paddle.move_paddle(Directions::Down);
+            }
+            transform.translation.y = paddle.position.y;
+        };
+   } 
+
+}
+
+fn hit_ball(paddles:Query<&mut Paddle>, mut balls:Query<&mut Ball>) {
+    let mut ball = balls.get_single_mut().unwrap();
+    for paddle in paddles.iter(){
+        if ball.position.x == paddle.position.x
+           && ball.position.y < (paddle.position.y + 120.0) && ball.position.y > (paddle.position.y - 120.0)
+           {
+            ball.bounce(Directions::Right);
+           }
+
+    }
+}
+
+
 fn setup(mut commands:Commands){
     commands.spawn(Camera2dBundle::default());
+    let commands = &mut commands;
     spawn_ball(commands);
+    spawn_players(commands);
 }
 
 fn ball_movement(mut query: Query<(&mut Ball,&mut Transform)>){
@@ -78,6 +148,24 @@ struct Vector2{
     x:f32,
     y:f32,
 }
+
+#[derive(Component)]
+struct Paddle{
+    position:Vector2,
+    speed:f32,
+    is_player:bool
+}
+
+impl Paddle{
+    fn move_paddle(&mut self, direction: Directions){
+        match direction{
+            Directions::Up => self.position.y += self.speed,
+            Directions::Down => self.position.y -= self.speed,
+            _ => {}
+        };
+    }
+}
+
 
 #[derive(Component)]
 struct Ball{
